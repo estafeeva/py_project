0dfa8aedfcf28ec1f7a0e95e280831a3fc86ac91
estafeeva <stafeeva.ea@mail.ru>
2024-01-21 16:25:18 +0300

    create subtraction_of_two

--- funcs.py
+++ funcs.py
@@ -4,3 +4,9 @@ def sum_of_two(a, b):
 
 print(sum_of_two(5, 6))
 
+def subtraction_of_two(a, b):
+    result_of_subtraction = a - b
+    return result_of_subtraction
+
+
+print(subtraction_of_two(50, 6))
